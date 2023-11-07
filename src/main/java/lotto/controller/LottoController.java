ab68936f5d2ed80eb38bb07a988b01627686490c
Hajin-Lee0406 <glgkwls1@naver.com>
2023-11-07 23:50:45 +0900

    Revert "feat: controller 실행 구조"
    This reverts commit e6f4e7fa9e551801451b9137eaff5005955c372d.

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,13 +1,4 @@
 package lotto.controller;
 
-import java.util.List;
-
 public class LottoController {
-    public void run() {
-        startLotto();
-    }
-
-    private void startLotto() {
-    }
-
 }
