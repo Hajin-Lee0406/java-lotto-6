adb38deabd6f4c318cc69b84823e9dfada948ee7
Hajin-Lee0406 <glgkwls1@naver.com>
2023-11-07 23:50:46 +0900

    Revert "feat: 등수 enum 생성"
    This reverts commit a00abc550d09ea83042d939861a013e77e7d4809.

--- src/main/java/lotto/model/Grade.java
+++ src/main/java/lotto/model/Grade.java
@@ -1,21 +1,4 @@
 package lotto.model;
 
-import static lotto.model.Constants.*;
-
-public enum Grade {
-    FIRST(6, 2000000000,RESULT_GRADE_FIRST),
-    SECOND(5,30000000, RESULT_GRADE_SECOND),
-    THIRD(5, 1500000, RESULT_GRADE_THIRD),
-    FOURTH(4, 50000, RESULT_GRADE_FOURTH),
-    FIFTH(3, 5000, RESULT_GRADE_FIFTH);
-
-    private final int hit;
-    private final int account;
-    private final String comment;
-
-    Grade(int hit, int account, String comment) {
-        this.hit = hit;
-        this.account = account;
-        this.comment = comment;
-    }
+public class Grade {
 }
